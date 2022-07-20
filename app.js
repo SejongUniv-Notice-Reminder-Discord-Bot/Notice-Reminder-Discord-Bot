971e33ec9e0032ea0985c8874100044c80992c64
KYUTAE PARK <rbxo0109@gmail.com>
2022-07-21 06:49:23 +0900

    진짜 최종본

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const tag = require("./tag.json");
 const fs = require('fs');
 const PB = require("./phonebook.json");
 const manual = require("./manual.json");
+let id;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -14,6 +15,7 @@ client.on('ready', () => {
 
 client.on('messageCreate', async (msg) => {
     const web1 = new web();
+    id = msg
     function fun() {
         return new Promise((res, rej) => {
             setTimeout(() => {
@@ -78,12 +80,6 @@ client.on('messageCreate', async (msg) => {
             .setTitle("교내 시설 전화번호 끝!!!!!!!");
         msg.channel.send({ embeds: [Embed1] });
 
-        /*
-        "department": "교무과",
-        "position": "직원",
-        "name": "박윤주",
-        "location": "집협관 101호",
-        "number": "02-3408-4192"*/
     }
     else if (msg.content === "!수강편람") {
         const Embed = new Discord.MessageEmbed()
@@ -102,29 +98,25 @@ client.on('messageCreate', async (msg) => {
 
 const refresh1 = new refresh();
 
-setInterval(function () {
-    setTimeout(async function () {
-        function readfun(step) {
-            return new Promise((res, rej) => {
-                setTimeout(() => {
-                    const temp = fs.readFileSync('./tag.json', 'utf8');
-                    arr = JSON.parse(temp);
-                    const Embed = new Discord.MessageEmbed()
-                        .setTitle(arr[step].subject)
-                        .setURL(arr[step].link)
-                    client.channels.cache.get('994189428903911476').send({ embeds: [Embed] });
-                }, 5000)
-            })
+setInterval(async function () {
+    function readfun() {
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                refresh1.refreshstart();
+            }, 5000)
+        })
+    }
+    readfun();
+    const temp = await fs.readFileSync('./tag.json', 'utf8');
+    arr = JSON.parse(temp);
+    for (let step = 0; step < 9; step++) {
+        if (arr[step].flag === "1") {
+            const Embed = new Discord.MessageEmbed()
+                .setTitle(arr[step].subject)
+                .setURL(arr[step].link)
+            client.channels.cache.get('994189428903911476').send({ embeds: [Embed] });
         }
-        const temp = fs.readFileSync('./tag.json', 'utf8');
-        arr = JSON.parse(temp);
-        refresh1.refreshstart();
-        for (step = 0; step < 9; step++) {
-            if (arr[step].flag === "1") {
-                readfun(step);
-            }
-        }
-    }, 3000);
+    }
 }, 36000000);
 
 client.login(token.token);
--- refresh.js
+++ refresh.js
@@ -1,12 +1,13 @@
 const client = require('cheerio-httpcli');
 const fs = require('fs');
-const tag = require('./tag.json');
 
 class refresh {
     refreshstart() {
         var arr = [];
         for (let step = 0; step < 9; step++) {
-            let url = `http://board.sejong.ac.kr/boardlist.do?bbsConfigFK=${tag[step].key}`;
+            const temp = fs.readFileSync('./tag.json', 'utf8');
+            const truarr = JSON.parse(temp)
+            let url = `http://board.sejong.ac.kr/boardlist.do?bbsConfigFK=${truarr[step].key}`;
             const param = {};
             client.set('headers', {
                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
@@ -18,25 +19,26 @@ class refresh {
                     return;
                 }
                 const key = "tr:nth-child(1)"
-                if (String($(key).find(".index").text()) !== tag[step].index) {
+                if ($(key).find(".index").text() !== truarr[step].index) {
                     arr.push({
-                        key: tag[step].key,
+                        key: truarr[step].key,
                         flag: "1",
                         index: $(key).find(".index").text(),
                         subject: $(key).find("td.subject > a").text().replaceAll('\t', '').replaceAll('\n', ''),
                         link: `http://board.sejong.ac.kr/${$(key).find("td.subject > a").attr("href")}`
                     })
+                    console.log(truarr[step].index, $(key).find(".index").text())
                 }
                 else {
                     arr.push({
-                        key: tag[step].key,
+                        key: truarr[step].key,
                         flag: "0",
                         index: $(key).find(".index").text(),
                         subject: $(key).find("td.subject > a").text().replaceAll('\t', '').replaceAll('\n', ''),
                         link: `http://board.sejong.ac.kr/${$(key).find("td.subject > a").attr("href")}`
                     })
+                    console.log(truarr[step].index, $(key).find(".index").text())
                 }
-
                 if (step === 8) {
                     const webJson = JSON.stringify(arr);
                     fs.writeFileSync('./tag.json', webJson);
